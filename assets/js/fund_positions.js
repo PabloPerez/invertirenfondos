1878ddec9dc1662ba3615defb7d0836604f024d1
Pablo Perez <p.perezcarrascal@gmail.com>
2022-08-07 19:36:11 +0200

    support for new and sold positions

--- assets/js/fund_positions.js
+++ assets/js/fund_positions.js
@@ -5,11 +5,13 @@ let fundData;
 populatePositions(isin);
 
 let pageWidth = window.innerWidth || document.body.clientWidth;
-let treshold = Math.max(1 , Math.floor(0.05 * (pageWidth)));
+let treshold = Math.max(1 , Math.floor(0.3 * (pageWidth)));
 let touchstartX = 0;
 let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
+
+let first = true;
 
 const limit = Math.tan(45 * 1.5 / 180 * Math.PI);
 const gestureZone = document.getElementById('positions-table');
@@ -25,6 +27,15 @@ gestureZone.addEventListener('touchend', function(event) {
     handleGesture(event);
 }, false);
 
+function showTrimester(trimester) {
+    [...document.querySelectorAll(".trimester")].forEach(e => document.body.removeChild(e));
+    const element = document.createElement("div");
+    element.innerText = trimester;
+    element.className = "trimester";
+    document.body.appendChild(element);
+    element.addEventListener("animationend", ()=>document.body.removeChild(element))
+}
+
 function handleGesture(e) {
     let x = touchendX - touchstartX;
     let y = touchendY - touchstartY;
@@ -33,9 +44,11 @@ function handleGesture(e) {
     if (Math.abs(x) > treshold || Math.abs(y) > treshold) {
         if (yx <= limit) {
             if (x < 0) {
-                populatePositions(isin, --pos)
+                pos--
+                populatePositions(isin, true)
             } else {
-                populatePositions(isin, ++pos) 
+                pos++
+                populatePositions(isin, true)
             }
         }
     } else {
@@ -45,10 +58,12 @@ function handleGesture(e) {
 
 
 document.querySelector('#previous-quarter').addEventListener("click", function () {
-    populatePositions(isin, ++pos) 
+    pos++
+    populatePositions(isin)
 })
 document.querySelector('#next-quarter').addEventListener("click", function() {
-    populatePositions(isin, --pos)
+    pos--
+    populatePositions(isin)
 })
 
 function getFundIsinFromUrl(url) {
@@ -57,7 +72,7 @@ function getFundIsinFromUrl(url) {
 
 }
 
-async function populatePositions(fundIsin) {
+async function populatePositions(fundIsin, isDragging=false) {
     if (pos >= 0){
         if (!fundData) {
             fundData = await axios.get(`../assets/json/funds/${fundIsin}.json`)
@@ -68,10 +83,15 @@ async function populatePositions(fundIsin) {
          }
         if (pos >= fundData.historico_periodos.length){
             pos = fundData.historico_periodos.length - 1;
+            return
         }
     } else {
         pos = 0;
+        return
     }
+
+    isDragging && !first && showTrimester(fundData.historico_periodos[pos].periodo)
+    first=false
 
     let turnoverRate = fundData.historico_periodos[pos].indice_rotacion;
     document.querySelector('#turnover-rate').innerHTML = turnoverRate;
@@ -98,13 +118,21 @@ async function populatePositions(fundIsin) {
     document.querySelector('#previous-quarter').style.visibility = "visible";
     document.querySelector('#next-quarter').style.visibility = "visible";    
 
+    let positions = fundData.historico_periodos[pos].posiciones;
+    let previous_positions = []
+    let previous_quarter
+
     if (pos == 0) {
         document.querySelector('#next-quarter').style.visibility = "hidden";
-    } if (pos == fundData.historico_periodos.length - 1) {
+    }
+    if (pos == fundData.historico_periodos.length - 1) {
         document.querySelector('#previous-quarter').style.visibility = "hidden";
-    } 
-
-    let positions = fundData.historico_periodos[pos].posiciones;
+        document.querySelector('#sold-positions').style.display = "none";
+    } else {
+        previous_quarter = fundData.historico_periodos[pos + 1].periodo
+        previous_positions = fundData.historico_periodos[pos + 1].posiciones
+        document.querySelector('#sold-positions').style.display = "block";
+    }
 
     tableBody = document.querySelector('#positions-table tbody')
     tableBody.innerHTML = ''
@@ -114,15 +142,19 @@ async function populatePositions(fundIsin) {
         let percentage = position.porcentaje;
         let isin = position.isin;
         let amount = position.cantidad;
+        let isNew = previous_positions.length && !previous_positions.some(p => p.isin === isin)
 
         let currency = position.moneda;
         if (currency.toLowerCase() == "euro" || currency.toLowerCase() == "eur") {
             currency = "€"
-        } else if (currency.toLowerCase() == "dolar") {
+        } else if (currency.toLowerCase() == "dolar" || currency.toLowerCase() == "dollar") {
             currency = "$"
         }
 
         let tr = document.createElement('tr');
+        if (isNew){
+            tr.classList.add("table-success")
+        }
 
         let nametd = document.createElement('td');
         nametd.innerText = name;
@@ -148,6 +180,40 @@ async function populatePositions(fundIsin) {
 
         tableBody.appendChild(tr)
     
+    });
+    soldTableBody = document.querySelector('#sold-positions-table tbody')
+    soldTableBody.innerHTML = ''
+    previous_positions.forEach(position => {
+        let isin = position.isin;
+        if (!positions.some(p => p.isin === isin)) {
+            let name = position.nombre;
+            let percentage = position.porcentaje;
+
+            let tr = document.createElement('tr');
+
+            let nametd = document.createElement('td');
+            nametd.innerText = name;
+            let isintd = document.createElement('td');
+            isintd.innerText = isin
+            isintd.classList.add("isintd")
+            let percentagetd = document.createElement('td');
+            percentagetd.classList.add("percentagetd")
+            percentagetd.innerText = percentage
+            let sincetd = document.createElement('td');
+            sincetd.classList.add("sincetd")
+            sincetd.innerText = position.desde
+
+            tr.addEventListener("click", function() {
+                window.location.href = `../activos/${isin}.html`
+            })
+
+            tr.appendChild(nametd);
+            tr.appendChild(isintd);
+            tr.appendChild(percentagetd);
+            tr.appendChild(sincetd);
+
+            soldTableBody.appendChild(tr)
+        }
     });
 
     document.querySelector('#quarter-date').innerHTML = fundData.historico_periodos[pos].periodo;
